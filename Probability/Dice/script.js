311d034c0c44abcdeed67b034faf8fa5d31ba8fa
Smashitter2020 <smashitter2020@gmail.com>
2025-07-17 15:05:30 -0700

    Update script.js

--- Probability/Dice/script.js
+++ Probability/Dice/script.js
@@ -1,15 +1,16 @@
-let num1;
+let num1, num2;
 
 function generateQuestion() {
-    num1 = Math.floor(Math.random() * 5) + 1;
-    document.getElementById("question").innerText = `What is the probability of rolling a six-sided dice greater than ${num1}?`;
+    num1 = Math.floor(Math.random() * 5) + 5;
+    num2 = Math.floor(Math.random() * num1) + 1;
+    document.getElementById("question").innerText = `What is the probability of rolling a ${num1}-sided dice greater than ${num2}?`;
     document.getElementById("result").innerText = "";
     document.getElementById("answer").value = "";
 }
 
 function checkAnswer() {
     const userAnswer = parseFloat(document.getElementById("answer").value);
-    const correctAnswer = ((6 - num1) / 6);
+    const correctAnswer = ((num1 - num2) / num1);
 
     if (Math.abs(userAnswer - correctAnswer) < 0.01) {
         document.getElementById("result").innerText = "Correct!";
