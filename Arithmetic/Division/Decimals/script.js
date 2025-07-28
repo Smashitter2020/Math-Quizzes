570d5f92b50f7b292e818c54bbc4759a02cd1f2b
Smashitter2020 <smashitter2020@gmail.com>
2025-07-27 21:53:01 -0700

    Update script.js

--- Arithmetic/Division/Decimals/script.js
+++ Arithmetic/Division/Decimals/script.js
@@ -3,7 +3,7 @@ let num1, num2;
 function generateQuestion() {
     num2 = (Math.floor(Math.random() * 1000) / 100) + 0.01;
     num1 = (Math.floor(Math.random() * 1000) / 100) + 0.01;
-    document.getElementById("question").innerText = `What is ${num1} ÷ ${num2}?`;
+    document.getElementById("question").innerHTML = `What is <math xmlns="http://www.w3.org/1998/Math/MathML"><mn>${num1}</mn><mo>&#xf7;</mo><mn>${num2}</mn></math>?`;
     document.getElementById("result").innerText = "";
     document.getElementById("answer").value = "";
 }
