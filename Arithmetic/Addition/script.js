3a8f752bcf27a103538f8db7a0edb63863186383
Smashitter2020 <smashitter2020@gmail.com>
2025-07-27 21:33:02 -0700

    Update script.js

--- Arithmetic/Addition/script.js
+++ Arithmetic/Addition/script.js
@@ -3,7 +3,7 @@ let num1, num2;
 function generateQuestion() {
     num1 = Math.floor(Math.random() * 100);
     num2 = Math.floor(Math.random() * 100);
-    document.getElementById("question").innerText = `What is ${num1} + ${num2}?`;
+    document.getElementById("question").innerText = `What is <math xmlns="http://www.w3.org/1998/Math/MathML"><mn>${num1}</mn><mo>+</mo><mn>${num2}</mn></math>?`;
     document.getElementById("result").innerText = "";
     document.getElementById("answer").value = "";
 }
