5c6332a9b95794f7a47b53bd5eff992500cb0c07
Smashitter2020 <smashitter2020@gmail.com>
2025-07-27 21:36:59 -0700

    Update script.js

--- Arithmetic/Multiplication/script.js
+++ Arithmetic/Multiplication/script.js
@@ -3,7 +3,7 @@ let num1, num2;
 function generateQuestion() {
     num1 = Math.floor(Math.random() * 12) + 1; // Limit to 1-12 for basic multiplication practice
     num2 = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("question").innerText = `What is <math xmlns="http://www.w3.org/1998/Math/MathML"><mn>${num1}</mn><mo>&#xd7;</mo><mn>${num2}</mn></math>?`;
+    document.getElementById("question").innerHTML = `What is <math xmlns="http://www.w3.org/1998/Math/MathML"><mn>${num1}</mn><mo>&#xd7;</mo><mn>${num2}</mn></math>?`;
     document.getElementById("result").innerText = "";
     document.getElementById("answer").value = "";
 }
