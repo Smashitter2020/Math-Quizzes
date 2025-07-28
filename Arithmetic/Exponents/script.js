456ba7ac288699341d81c79c8013f9df89544a75
Smashitter2020 <smashitter2020@gmail.com>
2025-07-27 21:43:40 -0700

    Update script.js

--- Arithmetic/Exponents/script.js
+++ Arithmetic/Exponents/script.js
@@ -3,7 +3,7 @@ let num1, num2;
 function generateQuestion() {
     num1 = Math.floor(Math.random() * 100);
     num2 = Math.floor(Math.random() * 100);
-    document.getElementById("question").innerText = `What is ${num1} to the power of ${num2}?`;
+    document.getElementById("question").inneHTML = `What is <math xmlns="http://www.w3.org/1998/Math/MathML"><msup><mn>${num1}</mn><mn>${num2}</mn></msup></math>?`;
     document.getElementById("result").innerText = "";
     document.getElementById("answer").value = "";
 }
