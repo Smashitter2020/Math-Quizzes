2309d412c31a7aa38e0e3752f7185753b4db1a29
Smashitter2020 <smashitter2020@gmail.com>
2025-07-27 21:37:12 -0700

    Update script.js

--- Arithmetic/Division/script.js
+++ Arithmetic/Division/script.js
@@ -3,7 +3,7 @@ let num1, num2;
 function generateQuestion() {
     num2 = Math.floor(Math.random() * 10) + 1; // Avoid division by zero
     num1 = num2 * (Math.floor(Math.random() * 10) + 1); // Ensure num1 is a multiple of num2
-    document.getElementById("question").innerText = `What is <math xmlns="http://www.w3.org/1998/Math/MathML"><mn>${num1}</mn><mo>&#xf7;</mo><mn>${num2}</mn></math>`;
+    document.getElementById("question").innerHTML = `What is <math xmlns="http://www.w3.org/1998/Math/MathML"><mn>${num1}</mn><mo>&#xf7;</mo><mn>${num2}</mn></math>`;
     document.getElementById("result").innerText = "";
     document.getElementById("answer").value = "";
 }
